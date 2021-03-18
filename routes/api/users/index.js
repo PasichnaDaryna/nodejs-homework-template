9caba53a713c3beb37a8024ec0b1d7ea1d4e160c
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2021-03-18 17:18:42 +0200

    create folders

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -8,7 +8,7 @@ const upload = require('../../../helpers/upload')
 router.post('/registration', userController.reg)
 router.post('/login', userController.login)
 router.post('/logout', guard, userController.logout)
-router.patch('/avatars', () => { })
+router.patch('/avatars', upload.single('avatar'), () => { })
 
 
 module.exports = router
