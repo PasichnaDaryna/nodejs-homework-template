e52886bc8000e09b9d366f483c9d2d46b8beca17
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2021-03-19 20:58:04 +0200

    avatars uploads

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const helmet = require('helmet')
+const path = require('path')
 
 const contactsRouter = require('./routes/api/contacts')
 const usersRouter = require('./routes/api/users')
 const app = express()
+require('dotenv').config()
+const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS
+app.use(express.static(path.join(__dirname, AVATARS_OF_USERS)))
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-
+app.use(helmet())
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
--- controllers/users.js
+++ controllers/users.js
@@ -5,10 +5,15 @@ const fs = require('fs').promises
 require('dotenv').config()
 const path = require('path')
 const Jimp = require('jimp')
+const { promisify } = require('util')
+const cloudinary = require('cloudinary').v2
 const SECRET_KEY = process.env.JWT_SECRET
 const { HttpCode } = require('../helpers/constants')
 const createFolderIsExist = require('../helpers/create-dir')
 
+
+
+const uploadCloud = promisify(cloudinary.uploader.upload)
 
 const reg = async (req, res, next) => {
     try {
@@ -74,19 +79,60 @@ const logout = async (req, res, next) => {
 const avatars = async (req, res, next) => {
     try {
         const id = req.user.id
-        const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS
-        const pathFile = req.file.pathFile
-        const newNameAvatar = `${Date.now()}-${req.file.originalName}`
-        const img = await Jimp.read(pathFile)
-        img.autocrop().cover(250, 200, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE,)
-            .writeAsync(pathFile)
-
-        await createFolderIsExist(path.join(AVATARS_OF_USERS, id))
-        await fs.rename(pathFile, path.join(AVATARS_OF_USERS, id, newNameAvatar))
-        const avatarUrl = path.normalize(path.join(id, newNameAvatar))
+        // const avatarUrl = await saveAvatarToStatic(req)
+        const {
+            public_id: imgIdCloud,
+            secure_url: avatarUrl,
+        } = await saveAvatarToCloud(req)
+        await Users.updateAvatar(id, avatarUrl, imgIdCloud)
+        return res.json({
+            status: 'success',
+            code: HttpCode.OK,
+            data: {
+                avatarUrl,
+            },
+        })
     } catch (e) {
         next(e)
     }
+}
+
+const saveAvatarToStatic = async (req) => {
+    const id = req.user.id
+    const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS
+    const pathFile = req.file.path
+    const newNameAvatar = `${Date.now()}-${req.file.originalname}`
+    const img = await Jimp.read(pathFile)
+    await img
+        .autocrop()
+        .cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE)
+        .writeAsync(pathFile)
+    await createFolderIsExist(path.join(AVATARS_OF_USERS, id))
+    await fs.rename(pathFile, path.join(AVATARS_OF_USERS, id, newNameAvatar))
+    const avatarUrl = path.normalize(path.join(id, newNameAvatar))
+    try {
+        await fs.unlink(path.join(process.cwd(), AVATARS_OF_USERS, req.user.avatar))
+    } catch (e) {
+        console.log(e.message)
+    }
+    return avatarUrl
+}
+
+const saveAvatarToCloud = async (req) => {
+    const pathFile = req.file.path
+    const result = await uploadCloud(pathFile, {
+        folder: 'Photo',
+        transformation: { width: 250, height: 250, crop: 'fill' },
+    })
+    cloudinary.uploader.destroy(req.user.imgIdCloud, (err, result) => {
+        console.log(err, result)
+    })
+    try {
+        await fs.unlink(pathFile)
+    } catch (e) {
+        console.log(e.message)
+    }
+    return result
 }
 
 module.exports = { reg, login, logout, avatars }
